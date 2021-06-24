89ad467e766ae194318a38e3c5f0a66fed11b1df
TerenceStark <xiashulang@gmail.com>
2021-06-24 17:07:27 +0800

    PriorityTable

---
+++ src/main/java/parser/common/PriorityTable.java
@@ -0,0 +1,26 @@
+package parser.common;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PriorityTable {
+    private List<List<String>> table = new ArrayList<>();
+
+    public PriorityTable() {
+        table.add(Arrays.asList("&", "|", "^"));
+        table.add(Arrays.asList("==", "!=", ">", "<", ">=", "<="));
+        table.add(Arrays.asList("+", "-"));
+        table.add(Arrays.asList("*", "/"));
+        table.add(Arrays.asList("<<", ">>"));
+    }
+
+    public int size(){
+        return table.size();
+    }
+
+    public List<String> get(int level) {
+        return table.get(level);
+    }
+
+}
