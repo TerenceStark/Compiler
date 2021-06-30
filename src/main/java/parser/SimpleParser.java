82f59ea971287c6120820e5fa7411441819d1a54
TerenceStark <xiashulang@gmail.com>
2021-07-01 00:29:43 +0800

    small changes

--- src/main/java/parser/SimpleParser.java
+++ src/main/java/parser/SimpleParser.java
@@ -5,24 +5,24 @@ import parser.common.ParseException;
 import parser.common.PeekTokenIterator;
 
 public class SimpleParser {
-
     // Expr -> digit + Expr | digit
     // digit -> 0|1|2|3|4|5|...|9
-    public static ASTNode parse(PeekTokenIterator iterator) throws ParseException {
+    public static ASTNode parse(PeekTokenIterator it) throws ParseException {
+
         var expr = new Expr();
-        var scalar = new Scalar(expr, iterator);
-        if (!iterator.hasNext()) {
-            return scalar;
+        var scalar = Factor.parse(it);
+        // base condition
+        if(!it.hasNext()){
+           return scalar;
         }
 
-        expr.setLexeme(iterator.peek());
-        iterator.matchNext("+");
+        expr.setLexeme(it.peek());
+        it.nextMatch("+");
         expr.setLabel("+");
         expr.addChild(scalar);
-        expr.setAstNodeType(ASTNodeTypes.BINARY_EXPR);
-        var rightNode = parse(iterator);
+        expr.setType(ASTNodeTypes.BINARY_EXPR);
+        var rightNode = parse(it);
         expr.addChild(rightNode);
         return expr;
     }
 }
-
