e81f4666cc6d7bea93e452609e05bda1bc0d3b5f
TerenceStark <xiashulang@gmail.com>
2021-07-02 00:36:34 +0800

    Translator Tests

---
+++ src/test/java/translator/BlockTest.java
@@ -0,0 +1,35 @@
+package translator;
+
+import lexer.common.LexicalException;
+import org.junit.jupiter.api.Test;
+import parser.Parser;
+import parser.common.ParseException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BlockTest {
+
+    @Test
+    public void test() throws LexicalException, ParseException {
+
+        var source = "var a = 1\n" +
+                "{\n" +
+                "var b = a * 100\n" +
+                "}\n" +
+                "{\n" +
+                "var b = a * 100\n" +
+                "}\n";
+        var ast = Parser.parse(source);
+        ast.print(0);
+        var translator = new Translator();
+        var program = translator.translate(ast);
+        System.out.println(program.toString());
+
+        assertEquals("a = 1\n" +
+                "p1 = a * 100\n" +
+                "b = p1\n" +
+                "p1 = a * 100\n" +
+                "b = p1", program.toString());
+    }
+
+}
