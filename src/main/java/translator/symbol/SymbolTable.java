d497bf89635256bb621b9514dc52e381660900b1
TerenceStark <xiashulang@gmail.com>
2021-07-02 00:56:47 +0800

    Translator.symbol

---
+++ src/main/java/translator/symbol/SymbolTable.java
@@ -0,0 +1,118 @@
+package translator.symbol;
+
+import lexer.Token;
+import lexer.TokenType;
+
+import java.util.ArrayList;
+
+public class SymbolTable {
+    private SymbolTable parent = null;
+    private ArrayList<SymbolTable> children;
+    private ArrayList<Symbol> symbols;
+    private int tempIndex = 0;
+    private int offsetIndex = 0;
+    private int level = 0;
+
+    public SymbolTable() {
+        this.children = new ArrayList<>();
+        this.symbols = new ArrayList<>();
+    }
+
+    public void addSymbol(Symbol symbol) {
+        this.symbols.add(symbol);
+        symbol.setParent(this);
+    }
+
+
+/*
+    var a = 1
+    {
+        {
+            {
+                var b = a
+            }
+        }
+    }
+*/
+    public Symbol cloneFromSymbolTree(Token lexeme, int layerOffset) {
+        var _symbol = this.symbols.stream()
+                .filter(x -> x.lexeme.get_value().equals(lexeme.get_value()))
+                .findFirst();
+        if (!_symbol.isEmpty()) {
+            var symbol = _symbol.get().copy();
+            symbol.setLayerOffset(layerOffset);
+            return symbol;
+        }
+        if (this.parent != null) {
+            return this.parent.cloneFromSymbolTree(lexeme, layerOffset + 1);
+        }
+        return null;
+    }
+
+    public boolean exists(Token lexeme) {
+        var _symbol = this.symbols.stream().filter(x -> x.lexeme.get_value().equals(lexeme.get_value())).findFirst();
+        if (!_symbol.isEmpty()) {
+            return true;
+        }
+        if (this.parent != null) {
+            return this.parent.exists(lexeme);
+        }
+        return false;
+    }
+
+    public Symbol createSymbolByLexeme(Token lexeme) {
+        Symbol symbol = null;
+        if (lexeme.isScalar()) {
+            symbol = Symbol.createImmediateSymbol(lexeme);
+            this.addSymbol(symbol);
+        } else {
+            var _symbol = this.symbols.stream().filter(x -> x.getLexeme().get_value().equals(lexeme.get_value())).findFirst();
+            if (_symbol.isEmpty()) {
+                symbol = cloneFromSymbolTree(lexeme, 0);
+                if (symbol == null) {
+                    symbol = Symbol.createAddressSymbol(lexeme, this.offsetIndex++);
+                }
+                this.addSymbol(symbol);
+            } else {
+                symbol = _symbol.get();
+            }
+        }
+        return symbol;
+    }
+
+    public Symbol createVariable() {
+        /*
+        * var a = 1 + 2 * 3
+        * p0 = 2 * 3
+        * p1 = 1 + p0
+        * */
+        var lexeme = new Token(TokenType.VARIABLE, "p" + this.tempIndex++);
+        var symbol = Symbol.createAddressSymbol(lexeme, this.offsetIndex++);
+        this.addSymbol(symbol);
+        return symbol;
+    }
+
+    public void addChild(SymbolTable child) {
+        child.parent = this;
+        child.level = this.level + 1;
+        this.children.add(child);
+    }
+
+    public int localSize() {
+        return this.offsetIndex;
+    }
+
+    public ArrayList<Symbol> getSymbols() {
+        return this.symbols;
+    }
+
+    public ArrayList<SymbolTable> getChildren() {
+        return this.children;
+    }
+
+
+    public void createLabel(String label, Token lexeme) {
+        var labelSymbol = Symbol.createLabelSymbol(label, lexeme);
+        this.addSymbol(labelSymbol);
+    }
+}
